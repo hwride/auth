fbe6dbdee0b9b397aa60db9c62e58afdece0eed3
Hugo Wride <hugowride@gmail.com>
2026-02-23 14:12:02 +0000

    Add basic Fastify server for client

--- oauth/client.ts
+++ oauth/client.ts
@@ -1,6 +1,25 @@
+// Import the framework and instantiate it
+import Fastify from "fastify";
+
 main();
 
-function main() {
-  const msg: string = "test";
-  console.log(msg);
+async function main() {
+  const fastify = await initServer();
+  fastify.log.info("Client is booted");
+}
+
+async function initServer() {
+  const fastify = Fastify({
+    logger: true,
+  });
+  fastify.get("/", async function handler(request, reply) {
+    return { hello: "world" };
+  });
+  try {
+    await fastify.listen({ port: 3000 });
+    return fastify;
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 }
