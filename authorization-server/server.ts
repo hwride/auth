becdd5f9a59c4f8eadebb706e11c762d06361801
Hugo Wride <hugowride@gmail.com>
2026-04-19 11:57:35 +0100

    Add fastify with hello world route

--- authorization-server/server.ts
+++ authorization-server/server.ts
@@ -1,1 +1,35 @@
-console.log("Hello world server!");
+import Fastify from "fastify";
+
+main();
+
+async function main() {
+  const fastify = await initServer();
+  fastify.log.info("Authorization server is booted");
+}
+
+async function initServer() {
+  const fastify = Fastify({
+    logger: {
+      transport: {
+        target: "pino-pretty",
+        options: {
+          translateTime: "HH:MM:ss Z",
+          ignore: "pid,hostname",
+        },
+      },
+    },
+    disableRequestLogging: true,
+  });
+
+  fastify.get("/", async () => {
+    return "hello world";
+  });
+
+  try {
+    await fastify.listen({ port: 4000 });
+    return fastify;
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
